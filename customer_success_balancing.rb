7441c11bcecd34d1fb8b5e86e32df25e5fa6d571
Johny Carebe <johny.carebe@gmail.com>
2023-01-30 19:36:23 -0300

    Make all scenarios passes

--- customer_success_balancing.rb
+++ customer_success_balancing.rb
@@ -53,51 +53,51 @@ class CustomerSuccessBalancingTests < Minitest::Test
     assert_equal 0, balancer.execute
   end
 
-  # def test_scenario_three
-  #   balancer = CustomerSuccessBalancing.new(
-  #     build_scores(Array(1..999)),
-  #     build_scores(Array.new(10000, 998)),
-  #     [999]
-  #   )
-  #   result = Timeout.timeout(1.0) { balancer.execute }
-  #   assert_equal 998, result
-  # end
+  def test_scenario_three
+    balancer = CustomerSuccessBalancing.new(
+      build_scores(Array(1..999)),
+      build_scores(Array.new(10000, 998)),
+      [999]
+    )
+    result = Timeout.timeout(1.0) { balancer.execute }
+    assert_equal 998, result
+  end
 
-  # def test_scenario_four
-  #   balancer = CustomerSuccessBalancing.new(
-  #     build_scores([1, 2, 3, 4, 5, 6]),
-  #     build_scores([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]),
-  #     []
-  #   )
-  #   assert_equal 0, balancer.execute
-  # end
+  def test_scenario_four
+    balancer = CustomerSuccessBalancing.new(
+      build_scores([1, 2, 3, 4, 5, 6]),
+      build_scores([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]),
+      []
+    )
+    assert_equal 0, balancer.execute
+  end
 
-  # def test_scenario_five
-  #   balancer = CustomerSuccessBalancing.new(
-  #     build_scores([100, 2, 3, 6, 4, 5]),
-  #     build_scores([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]),
-  #     []
-  #   )
-  #   assert_equal 1, balancer.execute
-  # end
+  def test_scenario_five
+    balancer = CustomerSuccessBalancing.new(
+      build_scores([100, 2, 3, 6, 4, 5]),
+      build_scores([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]),
+      []
+    )
+    assert_equal 1, balancer.execute
+  end
 
-  # def test_scenario_six
-  #   balancer = CustomerSuccessBalancing.new(
-  #     build_scores([100, 99, 88, 3, 4, 5]),
-  #     build_scores([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]),
-  #     [1, 3, 2]
-  #   )
-  #   assert_equal 0, balancer.execute
-  # end
+  def test_scenario_six
+    balancer = CustomerSuccessBalancing.new(
+      build_scores([100, 99, 88, 3, 4, 5]),
+      build_scores([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]),
+      [1, 3, 2]
+    )
+    assert_equal 0, balancer.execute
+  end
 
-  # def test_scenario_seven
-  #   balancer = CustomerSuccessBalancing.new(
-  #     build_scores([100, 99, 88, 3, 4, 5]),
-  #     build_scores([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]),
-  #     [4, 5, 6]
-  #   )
-  #   assert_equal 3, balancer.execute
-  # end
+  def test_scenario_seven
+    balancer = CustomerSuccessBalancing.new(
+      build_scores([100, 99, 88, 3, 4, 5]),
+      build_scores([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]),
+      [4, 5, 6]
+    )
+    assert_equal 3, balancer.execute
+  end
 
   private
 
