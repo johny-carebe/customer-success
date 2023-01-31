845c6dad884775676b959a8b4a4ea2a2ceda473e
Johny Carebe <johny.carebe@gmail.com>
2023-01-31 04:01:13 -0300

    Add docker and database configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,9 @@
-require "active_support/core_ext/integer/time"
+# frozen_string_literal: true
+
+require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
+  config.hosts.clear
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
@@ -14,18 +17,23 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # Allow access when running in docker
+  config.web_console.whitelisted_ips = ['172.16.0.0/12', '192.168.0.0/16', '172.20.0.1/12']
+
   # Enable server timing
   config.server_timing = true
 
+  config.web_console.permissions = '172.20.0.1'
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join("tmp/caching-dev.txt").exist?
+  if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      "Cache-Control" => "public, max-age=#{2.days.to_i}"
+      'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
@@ -38,6 +46,9 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+
+  # Care if the mailer can't send.
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.action_mailer.perform_caching = false
 
---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+rm -f /usr/src/app/tmp/pids/server.pid
+
+echo "bundle install..."
+bundle check || bundle install --jobs 4
+
+exec "$@"
